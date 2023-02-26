19b86ef7607f6762d0ec5da4ec29b1d24c48ab8e
Divya Sharma <divyasharma1106@gmail.com>
2023-02-26 23:01:31 +0530

    error handling for authentication

--- source/controllers/userAuth/signIn/signIn.ts
+++ source/controllers/userAuth/signIn/signIn.ts
@@ -2,24 +2,65 @@ import { Request, Response, NextFunction } from "express";
 import { SignIn } from "./signIn.Interface";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../config/firebase";
+import { FirebaseError } from "firebase/app";
 
 //Sign In Function
 const signIn = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    let response = {};
+
     const userDetails: SignIn = {
       email: req.body.email,
       password: req.body.password,
     };
+    
+    try {
+      const userSignIn = await signInWithEmailAndPassword(
+        auth,
+        userDetails.email,
+        userDetails.password
+      );
 
-    const userSignIn = await signInWithEmailAndPassword(
-      auth,
-      userDetails.email,
-      userDetails.password
-    );
+      response = {
+        success: 1,
+        message: "User was able to log in successfully."
+      }
+    } catch (error) {
+      console.log(error)
 
-    console.log("userSignIn", userSignIn);
+      if (error instanceof FirebaseError) {
+        if (error.code == "auth/wrong-password") {
+          response = {
+            success: 0,
+            error: "The entered password is a wrong password.",
+          };
+        } else if(error.code == "auth/user-not-found"){
+            response = {
+                success: 0,
+                error: "The user with this email ID does not exist.",
+            }
+        } else if (error.code == "auth/invalid-email"){
+            response = {
+                success: 0,
+                error: "Please enter a valid email ID."
+            }
+        } else{
+            response = {
+                success: 0,
+                error: error.code,
+            }
+        }
+      }
+    }
+    return res.send(response);
   } catch (error) {
     console.log(error);
+    const response = {
+      success: 0,
+      error: error,
+    };
+
+    return res.send(response);
   }
 };
 
--- source/controllers/userAuth/signUp/signUp.ts
+++ source/controllers/userAuth/signUp/signUp.ts
@@ -2,24 +2,64 @@ import { Request, Response, NextFunction } from "express";
 import { SignUp } from "./signUp.Interface";
 import { createUserWithEmailAndPassword} from "firebase/auth";
 import { auth } from "../../../config/firebase";
+import { FirebaseError } from "firebase/app";
 
 //Sign Up function
 const signUp = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    let response = {};
+
     const userDetails: SignUp = {
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
     };
 
-    const user = await createUserWithEmailAndPassword(
-      auth,
-      userDetails.email,
-      userDetails.password
-    );
-    console.log("user", user);
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        userDetails.email,
+        userDetails.password
+      );
+
+      response = {
+        success: 1,
+        message: "User was able to sign up successfully.",
+      };
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        if (error.code == "auth/email-already-in-use") {
+          response = {
+            success: 0,
+            error: "The provided email is already in use by an existing user.",
+          };
+        } else if (error.code == "auth/weak-password") {
+          response = {
+            success: 0,
+            error:
+              "The password is too weak. It should be at least 6 characters long.",
+          };
+        } else if (error.code == "auth/invalid-email") {
+          response = {
+            success: 0,
+            error: "Please enter a valid email ID.",
+          };
+        } else {
+          response = {
+            success: 0,
+            error: error.code,
+          };
+        }
+      }
+    }
+    return res.send(response);
   } catch (error) {
     console.log(error);
+    const response = {
+      success: 0,
+      error: error,
+    };
+    return res.send(response);
   }
 };
 
